b3f58f8dfdee02c37911ef346589032dde92195e
gevored <gersonrevoredo@hotmail.com>
2021-12-01 17:15:31 +0000

    request prismic done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,10 @@
+import style from './header.module.scss'
+
 export default function Header() {
   // TODO
+  return(
+    <div className= {style.container}>
+      <img src="/images/logo.svg" alt="logo" />
+    </div>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
-
+import {RichText} from 'prismic-dom';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Head from 'next/head'
+import Primisc from  '@prismicio/client'
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 interface Post {
   uid?: string;
@@ -24,13 +28,54 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+ export default function Home(props : HomeProps) {
+   // TODO
+   return (
+    <>
+      <Head>
+        <title>SpaceTraveling | Posts</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+    </>
+   )
+ }
 
-//   // TODO
-// };
+export const getStaticProps : GetStaticProps = async () => {
+
+   const prismic = getPrismicClient();
+
+   const postsResponse = await prismic.query(
+    Primisc.predicates.at('document.type', 'posts'),
+    {
+      pageSize : 2 
+    }
+   );
+
+   const constentsPost  = postsResponse.results.map(post=>{
+     return (
+       {
+         uid:post.uid,
+         first_publication_date:format( new Date(post.last_publication_date),'MM/dd/yyyy'),
+         data:{
+           title: post.data.title,
+           subtitle:post.data.subtitle,
+           author:post.data.author,
+          }
+        }
+        )
+      })
+      
+      const propReturn = {
+        next_page : postsResponse.next_page,
+        results : constentsPost
+      }
+  
+     // console.log(JSON.stringify(propReturn) , 2)
+  return{
+    props: {
+      propReturn,
+      
+    }
+  }
+  // TODO
+};
